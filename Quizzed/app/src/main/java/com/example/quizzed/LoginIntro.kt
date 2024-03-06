d0c97b19a2981aae6ff006ff7f40a690d0f0e713
Manjot Kaur <136496778+manjot9977@users.noreply.github.com>
2024-03-06 10:48:46 -0500

    update for authentication

---
+++ Quizzed/app/src/main/java/com/example/quizzed/LoginIntro.kt
@@ -0,0 +1,42 @@
+package com.example.quizzed
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.quizzed.databinding.ActivityLoginIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.lang.Exception
+
+class LoginIntro : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize the binding object using the layout inflater
+        binding = ActivityLoginIntroBinding.inflate(layoutInflater)
+        // Set the content view to the root view of the binding object
+        setContentView(binding.root)
+
+        val auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser !=null){
+            Toast.makeText(this,"User Already Logged in", Toast.LENGTH_SHORT).show()
+            redirect("MAIN")
+        }
+        // Access the views using binding object
+        binding.btnGetStarted.setOnClickListener{
+            redirect("LOGIN")
+        }
+    }
+
+    private fun redirect(name:String){
+        val intent = when(name){
+            "LOGIN" -> Intent(this, LoginActivity::class.java)
+            "MAIN" -> Intent(this, MainActivity::class.java)
+            else -> throw Exception ("no path exists")
+        }
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ Quizzed/app/src/main/java/com/example/quizzed/SignupActivity.kt
@@ -0,0 +1,61 @@
+package com.example.quizzed
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.quizzed.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignupActivity : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var binding: ActivitySignupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize the binding object using the layout inflater
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        // Set the content view to the root view of the binding object
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Access the views using binding object
+        binding.btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity ::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signUpUser(){
+        val email = binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        val confirmPassword = binding.etConfirmPassword.text.toString()
+
+        if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()){
+            Toast.makeText(this,"Email and Password can't Be Blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password != confirmPassword){
+            Toast.makeText(this,"Password didn't match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful){
+                    Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, SignupActivity::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this,"Error Creating User", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
